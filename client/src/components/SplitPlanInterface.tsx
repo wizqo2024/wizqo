039b623a6a0f0d91faedfac89732ceaac7957eb5
wizqo <wizqo2024@gmail.com>
2025-08-14 10:47:41 +0400

    fix: SplitPlanInterface now accepts required props for App.tsx

--- client/src/components/SplitPlanInterface.tsx
+++ client/src/components/SplitPlanInterface.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 
-export function SplitPlanInterface() {
+export interface QuizAnswers {
+  experience: string;
+  timeAvailable: string;
+  goal: string;
+}
+
+export interface PlanData {
+  [key: string]: any;
+}
+
+interface SplitPlanInterfaceProps {
+  onGeneratePlan: (hobby: string, answers: QuizAnswers, userId?: string, force?: boolean) => Promise<PlanData>;
+  onNavigateBack: () => void;
+  initialPlanData?: any;
+}
+
+export function SplitPlanInterface({
+  onGeneratePlan,
+  onNavigateBack,
+  initialPlanData,
+}: SplitPlanInterfaceProps) {
   return (
     <div>
       <h1>Split Plan Interface</h1>
-      {/* Add your actual UI code here */}
+      {/* You can use the props here */}
     </div>
   );
 }
