e09fb977f424c1a6787aec087858f7dafae8981d
wizqo <wizqo2024@gmail.com>
2025-08-14 10:45:28 +0400

    fix: make SplitPlanInterface.tsx a valid module export

---
+++ client/src/components/SplitPlanInterface.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function SplitPlanInterface() {
+  return (
+    <div>
+      <h1>Split Plan Interface</h1>
+      {/* Add your actual UI code here */}
+    </div>
+  );
+}
